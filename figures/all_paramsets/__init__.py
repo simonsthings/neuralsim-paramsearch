456f71392a5f759eab8507a3aa8b83fec89f83cf
Simon Vogt (using SourceTree on BCF-mbp) <simonsunimail@gmail.com>
2016-07-21 18:41:10 +0200

    part 2: bundling figure scripts by scope of image files they produce.
    - needed to do this in two separate steps in order to preserve history in OneParam.. file.

--- figures/all_paramsets/__init__.py
+++ figures/all_paramsets/__init__.py
@@ -2,4 +2,3 @@
 This package bundles all figure types that produce a summary over the whole simulation. That is, for all parameter sets and all of their repetitions.
 """
 import figuretype_OneParamAndRepetitions_Accuracy
-
